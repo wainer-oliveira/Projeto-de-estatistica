f0901b46a59b641352c3e9263a454cccc39aaa7a
Wainer97 <wainer.oliveira.luiz@gmail.com>
2020-06-05 22:48:20 -0300

    função de botão

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,49 @@
+let btn = document.getElementById('botao')
+
+//Acionado por clique do Botão
+btn.onclick = function pega_elementos(){
+
+    let tabela = document.getElementById("tipo").value
+    //Captura dos elementos do input
+    let elementsInput = document.getElementById("elementos")
+    const elementos = elementsInput.value.split(" ")
+    
+    //Reconhecendo qual tipo de tabela
+    switch(tabela){
+        case 'quali_nom' :
+            Qualitativa_Nominal(elementos)
+            break;
+        
+        case 'quali_ord' :
+            Qualitativa_Ordinal(elementos)
+            break;
+        
+        case 'quant_cont' :
+            Quantitativa_Continua(elementos)
+            break;
+        
+        case 'quant_disc' :
+            Quantitativa_Discreta(elementos)
+            break;
+    }
+    
+}
+
+
+function Qualitativa_Nominal(vetor){
+    alert('qualitativa Nominal teste')
+}
+
+function Qualitativa_Ordinal(vetor){
+    alert("Qualitativa Ordinal teste")
+}
+
+function Quantitativa_Continua(vetor){
+    alert('Quantitativa Continua teste')
+}
+
+function Quantitativa_Discreta(vetor){
+    alert('quantitativa Discreta teste')
+}
+
 
