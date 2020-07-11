06a88dfa7b62c5bdc849e05290f31d09ac999506
Evandro <evandro.gomes.tec@gmail.com>
2020-07-11 20:38:08 -0300

    Adição da classificação

--- assets/JS/correlacao.js
+++ assets/JS/correlacao.js
@@ -26,9 +26,23 @@ let r = (n * multXY - totalx * totaly) / Math.sqrt((n * x2 - totalx ** 2) * (n *
 preproX = (n * multXY - totalx * totaly) / (n * x2 - totalx ** 2)
 preproY = (totaly / n) - preproX * (totalx / n)
 
+if(r > 0.9 || r < -0.9){
+    classi = 'Muito Forte'
+}else if(r > 0.7 || r < -0.7){
+    classi = 'Forte'
+}else if(r > 0.5 || r < -0.5){
+    classi = 'Moderada'
+}else if(r > 0.3 || r < -0.3){
+    classi = 'Fraca'
+}else if(r > 0 || r < 0){
+    classi = 'Desprezíel'
+}else{
+    classi = 'Incompatível'
+}
 //=========================//   
 console.log(r)
 console.log(preproX) 
 console.log(preproY)
+console.log(classi)
 
 
